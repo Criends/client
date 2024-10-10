0a0e49f37e4471261d33c339dd8fcf7b91af5010
hwanieo <hwniieo@gmail.com>
2024-10-11 00:22:15 +0900

    feat: 헤더 구현

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,8 +3,8 @@ import NavLinks from "./NavLinks";
 
 export default function Header() {
   return (
-    <header className="fixed z-20 w-full backdrop-blur-sm drop-shadow-sm shadow-sm">
-      <div className="flex justify-between items-center px-40 py-4">
+    <header className="fixed z-20 w-full backdrop-blur-sm drop-shadow-sm shadow-sm px-40 py-2">
+      <div className="flex justify-between items-center">
         <div className="flex items-center gap-28">
           <Link
             href="/"
