36408204c9c9e5a27d3c0e495c7050c13a3de7c4
hwanieo <hwniieo@gmail.com>
2024-10-13 22:33:05 +0900

    feat: 헤더에 로그인 아이콘 추가

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,10 @@
 import Link from "next/link";
 import NavLinks from "./NavLinks";
+import { User } from "react-feather";
 
 export default function Header() {
   return (
-    <header className="fixed z-20 w-full backdrop-blur-sm drop-shadow-sm shadow-sm px-40 py-2">
+    <header className="fixed z-20 w-full backdrop-blur-sm drop-shadow-sm shadow-sm px-40 py-5">
       <div className="flex justify-between items-center">
         <div className="flex items-center gap-28">
           <Link
@@ -18,8 +19,9 @@ export default function Header() {
           </nav>
         </div>
 
-        <div className="w-20 py-2 bg-slate-200 rounded-full flex items-center justify-center">
-          login
+        <div className="px-4 py-2 bg-slate-200 rounded-full flex gap-1 items-center justify-center">
+          <User size={20} />
+          <span>Login</span>
         </div>
       </div>
     </header>
