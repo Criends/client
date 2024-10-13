96ae145b99cad2c09468b72e7a641c47deba0ba7
hwanieo <hwniieo@gmail.com>
2024-10-14 01:07:28 +0900

    feat: StartGuide에 배경 추가

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,6 +1,6 @@
 export default function Footer() {
   return (
-    <div className="w-full h-32 bg-red-50 absolute bottom-0 text-center pt-10">
+    <div className="w-full h-32 bg-brand text-lg text-white absolute bottom-0 text-center pt-10">
       Criends는 여러분들의 친구입니다.
     </div>
   );
