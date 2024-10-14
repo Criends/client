222a1d4dd71e090ac4a8a4e5a374225dd94aff76
hwanieo <hwniieo@gmail.com>
2024-10-14 23:43:08 +0900

    feat: 안내문구와 링크 적용

--- src/components/StartGuide.tsx
+++ src/components/StartGuide.tsx
@@ -1,3 +1,5 @@
+import Link from "next/link";
+
 export default function StartGuide() {
   return (
     <div className="px-10 md:px-0 py-44 flex flex-col text-center bg-slate-300 relative">
@@ -7,7 +9,7 @@ export default function StartGuide() {
       </div>
       <div className="relative">
         <h1 className="text-2xl md:text-3xl lg:text-4xl xl:4xl font-semibold text-white">
-          <pre className="text-brand inline-block tracking-tighter">
+          <pre className="text-brand inline-block tracking-tighter uppercase bg-white px-2 rounded-lg mr-2 hover:scale-110 transition-all hover:-rotate-2 cursor-pointer">
             Criends
           </pre>
           에서 당신의 이야기를 완성하세요!
@@ -18,12 +20,18 @@ export default function StartGuide() {
           눈에 띄는 이력서와 포트폴리오를 준비해보세요!
         </p>
         <div className="pt-14 flex flex-col md:flex-row justify-center gap-5 text-sm">
-          <button className="px-4 py-2 bg-slate-200 rounded-full">
+          <Link
+            href="/"
+            className="px-4 py-2 bg-brand text-white hover:bg-slate-200 hover:text-brand rounded-full transition-all"
+          >
             Criends와 함께하기
-          </button>
-          <button className="px-4 py-2 bg-slate-200 rounded-full">
+          </Link>
+          <Link
+            href="/"
+            className="px-4 py-2 bg-brand text-white hover:bg-slate-200 hover:text-brand rounded-full transition-all"
+          >
             Criends 둘러보기
-          </button>
+          </Link>
         </div>
       </div>
     </div>
