fef3aa4536e8340effda39a95431564aaa88f56f
hwanieo <hwniieo@gmail.com>
2024-10-14 22:16:26 +0900

    feat: StartGuide 버튼 위치 조정

--- src/components/StartGuide.tsx
+++ src/components/StartGuide.tsx
@@ -1,6 +1,6 @@
 export default function StartGuide() {
   return (
-    <div className="py-44 flex flex-col text-center bg-slate-300 relative">
+    <div className="px-10 md:px-0 py-44 flex flex-col text-center bg-slate-300 relative">
       <div className="absolute inset-0 flex justify-center items-center">
         <div className="w-[450px] h-[450px] bg-brand rounded-full opacity-50 blur-3xl"></div>
         {/* <div className="w-0 h-0 border-brand border-x-[600px] border-t-[600px] border-transparent border-t-brand opacity-50 blur-3xl"></div> */}
@@ -17,7 +17,7 @@ export default function StartGuide() {
           <br />
           눈에 띄는 이력서와 포트폴리오를 준비해보세요!
         </p>
-        <div className="pt-14 flex justify-center gap-5 text-sm">
+        <div className="pt-14 flex flex-col md:flex-row justify-center gap-5 text-sm">
           <button className="px-4 py-2 bg-slate-200 rounded-full">
             Criends와 함께하기
           </button>
