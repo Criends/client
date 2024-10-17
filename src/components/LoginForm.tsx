e789bf1270594690a683cd32912429847587a351
hwanieo <hwniieo@gmail.com>
2024-10-17 17:28:34 +0900

    feat: Main 컴포넌트 반응형 적용

--- src/components/LoginForm.tsx
+++ src/components/LoginForm.tsx
@@ -1,14 +1,23 @@
+"use client";
+
 export default function LoginForm() {
   return (
-    <form className="flex flex-col gap-2">
+    <form
+      className="flex flex-col gap-2 text-brand"
+      onSubmit={(event) => event.preventDefault()}
+    >
       <fieldset className="flex flex-col gap-2">
         <input
-          type="text"
+          type="email"
           className="bg-[#eeeeee] px-4 py-2 rounded-full outline-none"
+          placeholder="아이디를 입력하세요."
+          required
         />
         <input
           type="password"
           className="bg-[#eeeeee] px-4 py-2 rounded-full outline-none"
+          placeholder="비밀번호를 입력하세요."
+          required
         />
       </fieldset>
       <button className="bg-brand w-full px-2 py-2 rounded-full text-white">
